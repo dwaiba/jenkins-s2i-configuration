ce46df3b477f4b047714143c42558012c4105225
dwaiba <github@cloudgear.io>
2018-03-06 12:23:26 +0100

    'all in

--- nexus3/entrypoint.sh
+++ nexus3/entrypoint.sh
@@ -5,7 +5,8 @@
 
 # clear tmp and cache for upgrade
 #rm -fr "${NEXUS_DATA}"/tmp/ "${NEXUS_DATA}"/cache/
+#exec su -s /bin/sh -c '/opt/sonatype/nexus3/bin/nexus run' nexus3 || \
 
 [ $# -eq 0 ] && \
- exec su -s /bin/sh -c '/opt/sonatype/nexus3/bin/nexus run' nexus3 || \
+ exec  /bin/sh -c '/opt/sonatype/nexus3/bin/nexus run' nexus3 || \
  exec "$@"
